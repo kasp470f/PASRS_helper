b32fec6c89fa02c9af42e3565bdf6fa3c33c2b69
Caner Çalışkaner <caner.caliskaner@kindly.ai>
2025-05-20 19:30:46 +0200

    better build script

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
-mkdir -p build/firefox/dist;
-cp dist/main.js build/firefox/dist/main.js;
-cp dist/psd_replay.js build/firefox/dist/psd_replay.js;
-cp dist/room.js build/firefox/dist/room.js;
+function build() {
+    build_type=$1
+    mkdir -p build/$build_type/dist
+    cp dist/main.js build/$build_type/dist/main.js
+    cp dist/psd_replay.js build/$build_type/dist/psd_replay.js
+    cp dist/room.js build/$build_type/dist/room.js
 
-cp -r 128.png build/firefox/128.png;
-cp firefox_manifest.json build/firefox/manifest.json;
-pushd build/firefox;
-rm ../pasrs-helper.firefox.zip;
-zip -r ../pasrs-helper.firefox.zip .;
+    cp -r 128.png build/$build_type/128.png
+    if [ "$build_type" = "firefox" ]; then
+        cp firefox_manifest.json build/$build_type/manifest.json
+    else
+        cp manifest.json build/$build_type/manifest.json
+    fi
+    pushd build/$build_type
+    rm ../pasrs-helper.$build_type.zip
+    zip -r ../pasrs-helper.$build_type.zip .
+}
+
+build chrome
+build firefox

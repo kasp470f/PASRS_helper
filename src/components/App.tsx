0d43de03131593dfbee24b32ddeb21e5097b324c
Kasper Wissendorf <kw2011@live.dk>
2025-09-28 09:58:45 +0200

    feat: update App component layout and integrate Settings component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,21 @@
 import '../styles/common.scss';
+import Settings from './Settings/Settings';
 
 const App = () => {
 	return (
-		<div className="content">
-			<h1>Rsbuild with React</h1>
-			<p>Start building amazing things with Rsbuild.</p>
+		<div className='pasrs-app'>
+			<header>
+				<h1>PASRS Helper</h1>
+				<h2>A tool to help record your replays for faster processing of PASRS</h2>
+			</header>
+			<section className='content'>
+				<section className='replay-section'>
+
+				</section>
+				<section className='settings-section'>
+					<Settings />
+				</section>
+			</section>
 		</div>
 	);
 };

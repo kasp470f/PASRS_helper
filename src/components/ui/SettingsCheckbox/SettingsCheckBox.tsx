6b65ee0ede618f6ba05885cfbc751a55e18193c2
Kasper Wissendorf <kw2011@live.dk>
2025-09-28 09:58:37 +0200

    feat: add SettingsCheckbox component with styling

---
+++ src/components/ui/SettingsCheckbox/SettingsCheckBox.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SettingsKey } from '../../../types/settings';
+import './SettingsCheckBox.scss';
+
+
+interface SettingsCheckboxProps {
+	settingsKey: SettingsKey;
+	label: string;
+	checked: boolean;
+	onChange: (key: SettingsKey, value: boolean) => void;
+	disabled?: boolean;
+}
+
+const SettingsCheckbox: React.FC<SettingsCheckboxProps> = ({
+	settingsKey,
+	label,
+	checked,
+	onChange,
+	disabled
+}) => {
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		onChange(settingsKey, event.target.checked);
+	};
+
+	return (
+		<div className="settings-checkbox">
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={handleChange}
+				disabled={disabled}
+			/>
+			<span>{label}</span>
+		</div>
+	);
+};
+
+export default SettingsCheckbox;
